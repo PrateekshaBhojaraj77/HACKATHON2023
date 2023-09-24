f8619ce549988ef8c30adf4a9af21e08f569d3d1
sricharanmohankumar <S.MohanKumar001@umb.edu>
2023-09-24 15:40:42 -0400

    adding training data

--- image.py
+++ image.py
@@ -14,7 +14,8 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 id = 0
 
 # names related to ids: example ==> Marcelo: id=1,  etc
-names = ['None', 'Shreyas']
+names = ['None','sricharan','prateeksha','sammed']
+
 
 # Initialize and start realtime video capture
 cam = cv2.VideoCapture(0)
